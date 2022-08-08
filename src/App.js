2e60a9c57a7886ceaefd5829704a9e6352de0270
AndriySuprunenko <43989070+AndriySuprunenko@users.noreply.github.com>
2022-08-08 21:12:48 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import Container from "@mui/material/Container";
+import Container from '@mui/material/Container';
 
-import { Header } from "./components";
-import { Home, FullPost, Registration, AddPost, Login } from "./pages";
+import { Header } from './components';
+import { Home, FullPost, Registration, AddPost, Login } from './pages';
 
 function App() {
   return (
