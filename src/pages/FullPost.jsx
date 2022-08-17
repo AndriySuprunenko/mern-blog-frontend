13ff84973dcd9d52d029d087f16b9b264f8c8039
AndriySuprunenko <43989070+AndriySuprunenko@users.noreply.github.com>
2022-08-18 01:48:50 +0300

    update

--- src/pages/FullPost.jsx
+++ src/pages/FullPost.jsx
@@ -1,50 +1,62 @@
-import React from "react";
+import React from 'react';
+import { useParams } from 'react-router-dom';
 
-import { Post } from "../components/Post";
-import { Index } from "../components/AddComment";
-import { CommentsBlock } from "../components/CommentsBlock";
+import { Post } from '../components/Post';
+import { Index } from '../components/AddComment';
+import { CommentsBlock } from '../components/CommentsBlock';
+import axios from 'axios';
 
 export const FullPost = () => {
+  const [data, setData] = React.useState();
+  const [isLoading, setLoading] = React.useState(true);
+  const { id } = useParams();
+
+  React.useEffect(() => {
+    axios
+      .get(`/posts/${id}`)
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert('Помилка при отриманні статті');
+      });
+  });
+
+  if (isLoading) {
+    return <Post isLoading={isLoading} isFullPost />;
+  }
+
   return (
     <>
       <Post
-        id={1}
-        title="Roast the code #1 | Rock Paper Scissors"
+        id={data._id}
+        title={data.title}
         imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
-        user={{
-          avatarUrl:
-            "https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png",
-          fullName: "Keff",
-        }}
-        createdAt={"12 червень 2022 г."}
-        viewsCount={150}
+        user={data.user}
+        createdAt={data.createdAt}
+        viewsCount={data.viewsCount}
         commentsCount={3}
-        tags={["react", "fun", "typescript"]}
+        tags={data.tags}
         isFullPost
       >
-        <p>
-          Hey there! 👋 I'm starting a new series called "Roast the Code", where
-          I will share some code, and let YOU roast and improve it. There's not
-          much more to it, just be polite and constructive, this is an exercise
-          so we can all learn together. Now then, head over to the repo and
-          roast as hard as you can!!
-        </p>
+        <p>{data.text}</p>
       </Post>
       <CommentsBlock
         items={[
           {
             user: {
-              fullName: "Андрій Супруненко",
-              avatarUrl: "https://mui.com/static/images/avatar/1.jpg",
+              fullName: 'Андрій Супруненко',
+              avatarUrl: 'https://mui.com/static/images/avatar/1.jpg',
             },
-            text: "Це тестовий коментар))",
+            text: 'Це тестовий коментар))',
           },
           {
             user: {
-              fullName: "Василь Петренко",
-              avatarUrl: "https://mui.com/static/images/avatar/2.jpg",
+              fullName: 'Василь Петренко',
+              avatarUrl: 'https://mui.com/static/images/avatar/2.jpg',
             },
-            text: "When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top",
+            text: 'When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top',
           },
         ]}
         isLoading={false}
